7230dc66829504615599e0102c4e655cc27939fc
Mayowa Olajide <olajidemayorwa@gmail.com>
2025-12-20 11:28:53 +0100

    few updates on the config server

---
+++ ConfigServer/src/test/java/com/micromart/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micromart.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
