c4607394ab8c0432563a58c9df9e1f811a87d7da
Mayorman07 <olajidemayorwa@gmail.com>
2025-12-26 20:16:39 +0100

    added rabbit mq dependencies

---
+++ Users/src/main/java/com/micromart/configuration/RabbitMQConfig.java
@@ -0,0 +1,19 @@
+package com.micromart.configuration;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    TopicExchange userEventsExchange() {
+        return new TopicExchange("user-events-exchange");
+    }
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
