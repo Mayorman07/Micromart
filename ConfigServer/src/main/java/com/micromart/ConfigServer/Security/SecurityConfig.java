468f7975808433ee76ff75498e9649fe563fbe84
Mayorman07 <olajidemayorwa@gmail.com>
2025-12-30 12:21:00 +0100

    change to security config for config server

--- ConfigServer/src/main/java/com/micromart/ConfigServer/Security/SecurityConfig.java
+++ ConfigServer/src/main/java/com/micromart/ConfigServer/Security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.micromart.ConfigServer.Security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
