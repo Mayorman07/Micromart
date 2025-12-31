bc7bf46fa18753388d1ace379f4507a46053b047
Mayorman07 <olajidemayorwa@gmail.com>
2025-12-31 11:52:56 +0100

    added flyway database migration

--- Users/src/main/java/com/micromart/entities/User.java
+++ Users/src/main/java/com/micromart/entities/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable {
     private String email;
     @Column(nullable = false,unique = true)
     private String userId;
-    @Column(nullable = false,unique = true)
+    @Column(nullable = false)
     private String encryptedPassword;
     private String gender;
     private Date lastLoggedIn;
