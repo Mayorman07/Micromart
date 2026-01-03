fad0fbae99fa85f1870f2810781f4d703d80da8b
Mayorman07 <olajidemayorwa@gmail.com>
2026-01-03 01:58:00 +0100

    added authority entity class

--- Users/src/main/java/com/micromart/entities/Role.java
+++ Users/src/main/java/com/micromart/entities/Role.java
@@ -1,13 +1,19 @@
 package com.micromart.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import java.io.Serializable;
+import java.util.Collection;
 
 @Entity
 @Table(name="roles")
@@ -21,4 +27,12 @@ public class Role implements Serializable {
     private long id;
     @Column(nullable = false, length = 23)
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    @ManyToMany(cascade = CascadeType.PERSIST ,fetch = FetchType.EAGER )
+    @JoinTable(name="roles_authorities", joinColumns =@JoinColumn(name = "roles_id", referencedColumnName = "id"),inverseJoinColumns
+            = @JoinColumn(name = "authorities_id", referencedColumnName = "id"))
+    private Collection<Authority> authorities;
 }
