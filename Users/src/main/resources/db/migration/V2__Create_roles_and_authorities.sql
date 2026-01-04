a57124d52b02a812dc2df391d8c17844e4f8fe50
Mayorman07 <olajidemayorwa@gmail.com>
2026-01-04 21:05:58 +0100

    updated flyway script

--- Users/src/main/resources/db/migration/V2__Create_roles_and_authorities.sql
+++ Users/src/main/resources/db/migration/V2__Create_roles_and_authorities.sql
@@ -8,12 +8,11 @@ CREATE TABLE roles (
 -- 2. Create AUTHORITIES Table
 CREATE TABLE authorities (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL, -- Increased size to 50 to handle "product:WRITE"
+    name VARCHAR(50) NOT NULL,
     CONSTRAINT uk_authorities_name UNIQUE (name)
 );
 
 -- 3. Create ROLES_AUTHORITIES (Junction Table)
--- Links Roles to Authorities (e.g., MANAGER has product:WRITE)
 CREATE TABLE roles_authorities (
     role_id BIGINT NOT NULL,
     authority_id BIGINT NOT NULL,
@@ -22,8 +21,6 @@ CREATE TABLE roles_authorities (
 );
 
 -- 4. Create USERS_ROLES (Junction Table)
--- Links your existing Users table to the new Roles table
--- Note: It uses 'user_id' referring to the BIGINT id, NOT the UUID string
 CREATE TABLE users_roles (
     user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
