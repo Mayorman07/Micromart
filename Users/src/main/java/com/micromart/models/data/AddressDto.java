0112cb2811671eb5a919df8491b67d2d2e8c1ec3
Mayorman07 <olajidemayorwa@gmail.com>
2026-01-19 23:38:02 +0100

    a few class changes

---
+++ Users/src/main/java/com/micromart/models/data/AddressDto.java
@@ -0,0 +1,26 @@
+package com.micromart.models.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddressDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String addressId;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+    private String type;       // e.g., "SHIPPING", "BILLING"
+}
--- Users/src/main/java/com/micromart/models/data/UserDto.java
+++ Users/src/main/java/com/micromart/models/data/UserDto.java
@@ -1,10 +1,12 @@
 package com.micromart.models.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.micromart.constants.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 @Data
@@ -12,12 +14,12 @@ import java.util.List;
 @NoArgsConstructor
 public class UserDto implements Serializable {
 
+    @Serial
     private  static final long serialVersionUID = -953297098295050686L;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
-    private String address;
     private String mobileNumber;
     private String userId;
     private String encryptedPassword;
@@ -26,4 +28,8 @@ public class UserDto implements Serializable {
     private String createdAt;
     private Status status;
     private List<String> roles;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private AddressDto address;
+    private List<AddressDto> addresses;
+
 }
