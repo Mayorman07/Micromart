18ab9ed79495abca1018f86ca9221c264476336f
Mayorman07 <olajidemayorwa@gmail.com>
2026-01-27 01:09:23 +0100

    validation constraints for reset password

--- Users/src/main/java/com/micromart/models/requests/PasswordResetRequest.java
+++ Users/src/main/java/com/micromart/models/requests/PasswordResetRequest.java
@@ -1,10 +1,14 @@
 package com.micromart.models.requests;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PasswordResetRequest {
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",
+            message = "Password must be 8+ chars with 1 upper, 1 lower, 1 number, and 1 special char")
     private String email;
 }
