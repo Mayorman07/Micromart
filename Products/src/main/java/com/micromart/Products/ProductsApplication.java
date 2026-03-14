e41ddbfa220f1ad7f5546c7d3e00539925723fed
Mayorman07 <olajidemayorwa@gmail.com>
2026-03-14 12:09:12 +0100

    fix(backend): resolve PageImpl serialization warning in products service

--- Products/src/main/java/com/micromart/Products/ProductsApplication.java
+++ Products/src/main/java/com/micromart/Products/ProductsApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ProductsApplication {
 
 	public static void main(String[] args) {
